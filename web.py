6a64503bde5ff9d2ca5d6cbbdffb439364261a1d
mahdimizabiasl021-crypto <mahdi.mizabi.asl.021@gmail.com>
2025-12-24 22:58:13 +0330

    web.py

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import MKQ55596
 import asyncio
+import os   # ← اضافه شد
 
 app = Flask(__name__)
 
@@ -10,6 +11,5 @@ def home():
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    # ربات را داخل event loop اصلی اجرا می‌کنیم
     loop.create_task(MKQ55596.run_bot())
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
